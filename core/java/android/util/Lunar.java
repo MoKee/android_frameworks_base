d138fd41ae7fcb4b8027e98640d05e9ce6159e10
martincz <martincz@mokeetek.com>
2012-12-21 18:14:14 +0800

    add more chinadate string support
    Change-Id: I5d554b15fce08ecc20e1ed93c44cc6ffc78d09cc

--- core/java/android/util/Lunar.java
+++ core/java/android/util/Lunar.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2012 The MoKee Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package android.util;
 
 import java.text.ParseException;
@@ -167,7 +184,7 @@ public class Lunar {
     	String year1 = mContext.getResources().getString(R.string.status_year);
     	String run1 = mContext.getResources().getString(R.string.status_leap);
     	String month1 = mContext.getResources().getString(R.string.status_month);
-        return animalsYear() + year1 + (leap ? run1 : "") + chineseNumber[month - 1] + month1
+        return cyclical() + animalsYear() + year1 + (leap ? run1 : "") + chineseNumber[month - 1] + month1
                 + getChinaDayString(day);
     }
 }
