d16968c0a96750c1de04ad31c54f9f57b4afe824
martincz <martincz@mokeetek.com>
2012-12-29 20:49:30 +0800

    rename to ro.mk.version
    Change-Id: I5bfc89ac0a378ac107c96a3d1fe993e985a51940

--- core/java/android/webkit/WebSettingsClassic.java
+++ core/java/android/webkit/WebSettingsClassic.java
@@ -433,9 +433,9 @@ public class WebSettingsClassic extends WebSettings {
             buffer.append(" Build/");
             buffer.append(id);
         }
-        final String cmversion = SystemProperties.get("ro.cm.version");
-        if (cmversion != null && cmversion.length() > 0)
-            buffer.append("; CyanogenMod-" + cmversion.replaceAll("([0-9\\.]+?)-.*","$1"));
+        final String mkversion = SystemProperties.get("ro.mk.version");
+        if (mkversion != null && mkversion.length() > 0)
+            buffer.append("; MoKee-" + mkversion.replaceAll("([0-9\\.]+?)-.*","$1"));
         String mobile = context.getResources().getText(
             com.android.internal.R.string.web_user_agent_target_content).toString();
         final String base = context.getResources().getText(
