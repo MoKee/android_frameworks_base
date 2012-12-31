f6abc383fb20ee956f08db523aa9a897483b8ab7
martincz <martincz@mokeetek.com>
2012-12-31 13:30:44 +0800

    fix 24hours bind bug
    Change-Id: I6a7d0338eb0dec625a8b691a13a94d97ae5f3f07

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -173,7 +173,7 @@ public class Clock extends TextView implements OnClickListener, OnLongClickListe
         boolean b24 = DateFormat.is24HourFormat(context);
         int res;
 
-        if (b24) {
+        if (b24 || AM_PM_STYLE == AM_PM_STYLE_GONE) {
             res = R.string.twenty_four_hour_time_format;
         } else {
             res = R.string.twelve_hour_time_format;
