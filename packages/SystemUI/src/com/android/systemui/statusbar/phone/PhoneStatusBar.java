9eae11ec1f71998e848aeb6bea4905091654eb1e
Robert Burns <burnsra@gmail.com>
2013-01-09 18:03:46 -0500

    Add Quick Settings USB Tether tile (2 of 2)
    Patch Set 8: Make tile dynamic
    
    - Thanks to Björn Lundén for the drawables
    
    Change-Id: I414848f6cb8a789d0b5edfcb79ab53cb85ebc20e

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -2887,6 +2887,10 @@ public class PhoneStatusBar extends BaseStatusBar {
                     false, this);
 
             cr.registerContentObserver(
+                    Settings.System.getUriFor(Settings.System.QS_DYNAMIC_USBTETHER),
+                    false, this);
+
+            cr.registerContentObserver(
                     Settings.System.getUriFor(Settings.System.QS_DYNAMIC_WIFI),
                     false, this);
         }
