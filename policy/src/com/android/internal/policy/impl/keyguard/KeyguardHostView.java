7f4316527b027f04fa09c81215d4c5fa624c6ebb
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2013-01-20 08:26:38 -0500

    Lockscreen: longpress on expand challenge handle
    - Unsecure screen lock, long press will unlock the device
    - Secure screen lock, long press and widget will fade into the background and secure method is displayed
    
    Change-Id: I7d980f8e7e07226c97a64a63910a1410e6fb84c0

--- policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
+++ policy/src/com/android/internal/policy/impl/keyguard/KeyguardHostView.java
@@ -33,14 +33,10 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentSender;
-import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.pm.UserInfo;
 import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Rect;
-import android.graphics.drawable.BitmapDrawable;
 import android.os.Looper;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -110,6 +106,10 @@ public class KeyguardHostView extends KeyguardViewBase {
 
     private boolean mSafeModeEnabled;
 
+
+     // We can use the profile manager to override security
+     private ProfileManager mProfileManager;
+
      /*package*/ interface TransportCallback {
         void onListenerDetached();
         void onListenerAttached();
@@ -141,6 +141,8 @@ public class KeyguardHostView extends KeyguardViewBase {
         mSecurityModel = new KeyguardSecurityModel(context);
 
         mViewStateManager = new KeyguardViewStateManager(this);
+
+        mProfileManager = (ProfileManager) context.getSystemService(Context.PROFILE_SERVICE);
 
         DevicePolicyManager dpm =
             (DevicePolicyManager) mContext.getSystemService(Context.DEVICE_POLICY_SERVICE);
@@ -237,7 +239,6 @@ public class KeyguardHostView extends KeyguardViewBase {
             setSystemUiVisibility(getSystemUiVisibility() | View.STATUS_BAR_DISABLE_BACK);
         }
 
-        updateBackground();
         addDefaultWidgets();
 
         addWidgetsFromSettings();
@@ -271,32 +272,6 @@ public class KeyguardHostView extends KeyguardViewBase {
             return true;
         }
     };
-
-    private void updateBackground() {
-        String background = Settings.System.getStringForUser(getContext().getContentResolver(),
-                Settings.System.LOCKSCREEN_BACKGROUND, UserHandle.USER_CURRENT);
-
-        if (background == null) {
-            return;
-        }
-
-        if (!background.isEmpty()) {
-            try {
-                setBackgroundColor(Integer.parseInt(background));
-            } catch(NumberFormatException e) {
-                Log.e(TAG, "Invalid background color " + background);
-            }
-        } else {
-            try {
-                Context settingsContext = getContext().createPackageContext("com.android.settings", 0);
-                String wallpaperFile = settingsContext.getFilesDir() + "/lockwallpaper";
-                Bitmap backgroundBitmap = BitmapFactory.decodeFile(wallpaperFile);
-                setBackgroundDrawable(new BitmapDrawable(backgroundBitmap));
-            } catch (NameNotFoundException e) {
-            // Do nothing here
-            }
-        }
-    }
 
     private int getDisabledFeatures(DevicePolicyManager dpm) {
         int disabledFeatures = DevicePolicyManager.KEYGUARD_DISABLE_FEATURES_NONE;
@@ -908,11 +883,31 @@ public class KeyguardHostView extends KeyguardViewBase {
         showPrimarySecurityScreen(false);
     }
 
+    private boolean isSecure() {
+        SecurityMode mode = mSecurityModel.getSecurityMode();
+        switch (mode) {
+            case Pattern:
+                return mLockPatternUtils.isLockPatternEnabled()
+                        && mProfileManager.getActiveProfile().getScreenLockMode()!= Profile.LockMode.INSECURE;
+            case Password:
+            case PIN:
+                return mLockPatternUtils.isLockPasswordEnabled()
+                        && mProfileManager.getActiveProfile().getScreenLockMode() != Profile.LockMode.INSECURE;
+            case SimPin:
+            case SimPuk:
+            case Account:
+                return true;
+            case None:
+                return false;
+            default:
+                throw new IllegalStateException("Unknown security mode " + mode);
+        }
+    }
 
     @Override
     public void wakeWhenReadyTq(int keyCode) {
         if (DEBUG) Log.d(TAG, "onWakeKey");
-        if (keyCode == KeyEvent.KEYCODE_MENU && mSecurityModel.getSecurityMode() != SecurityMode.None) {
+        if (keyCode == KeyEvent.KEYCODE_MENU && isSecure()) {
             if (DEBUG) Log.d(TAG, "switching screens to unlock screen because wake key was MENU");
             showSecurityScreen(SecurityMode.None);
         } else {
