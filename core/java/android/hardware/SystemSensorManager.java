85975f287c1f01547a315e49cccc39d8ea6356a5
xilence2210 <xilence2210@gmail.com>
2013-03-24 11:37:03 +0800

    Decrease lag generated by autorotation on
    Change-Id: Ia9d3c56a633cf8b871a090ae62c3e8b25bfe586d

--- core/java/android/hardware/SystemSensorManager.java
+++ core/java/android/hardware/SystemSensorManager.java
@@ -24,6 +24,8 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.util.SparseBooleanArray;
 import android.util.SparseIntArray;
+import android.os.SystemProperties;
+import android.util.FloatMath;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -36,6 +38,11 @@ import java.util.List;
  */
 public class SystemSensorManager extends SensorManager {
     private static final int SENSOR_DISABLE = -1;
+
+    // minimal duration of a sensor loop => sleep to prevent notification storm if faster than that.
+    private static final long SENSOR_LOOPMINMS = SystemProperties.getLong( "sensor.loop.minms", 0); 
+    private static final float MAGNITUDE_THRESHOLD = ((float)SystemProperties.getLong( "sensor.magnitude.threshold", 0))/1000.0f;
+
     private static boolean sSensorModuleInitialized = false;
     private static ArrayList<Sensor> sFullSensorsList = new ArrayList<Sensor>();
     /* The thread and the sensor list are global to the process
@@ -121,6 +128,9 @@ public class SystemSensorManager extends SensorManager {
                     // wait for an event
                     final int sensor = sensors_data_poll(sQueue, values, status, timestamp);
 
+                    long lastLoopMS = System.currentTimeMillis();
+                    long timeToSleep = SENSOR_LOOPMINMS;
+
                     int accuracy = status[0];
                     synchronized (sListeners) {
                         if (sensor == -1 || sListeners.isEmpty()) {
@@ -138,20 +148,29 @@ public class SystemSensorManager extends SensorManager {
                         }
                         final Sensor sensorObject = sHandleToSensor.get(sensor);
                         if (sensorObject != null) {
-                            // report the sensor event to all listeners that
-                            // care about it.
-                            final int size = sListeners.size();
-                            for (int i=0 ; i<size ; i++) {
-                                ListenerDelegate listener = sListeners.get(i);
-                                if (listener.hasSensor(sensorObject)) {
-                                    // this is asynchronous (okay to call
-                                    // with sListeners lock held).
-                                    listener.onSensorChangedLocked(sensorObject,
-                                            values, timestamp, accuracy);
+                            final float mag = FloatMath.sqrt(values[0] * values[0] + values[1] * values[1] + values[2] * values[2]);
+                            if (Math.abs(mag - SensorManager.STANDARD_GRAVITY) > MAGNITUDE_THRESHOLD) {
+                                // report the sensor event to all listeners that
+                                // care about it.
+                                final int size = sListeners.size();
+                                for (int i=0 ; i<size ; i++) {
+                                    ListenerDelegate listener = sListeners.get(i);
+                                    if (listener.hasSensor(sensorObject)) {
+                                        // this is asynchronous (okay to call
+                                        // with sListeners lock held).
+                                        listener.onSensorChangedLocked(sensorObject,
+                                                values, timestamp, accuracy);
+                                    }
                                 }
                             }
                         }
                     }
+
+                    timeToSleep = SENSOR_LOOPMINMS - (System.currentTimeMillis() - lastLoopMS);
+                    lastLoopMS = System.currentTimeMillis();
+                    try { if(timeToSleep>0) Thread.sleep(timeToSleep);}
+                    catch(InterruptedException e) {;}
+
                 }
                 //Log.d(TAG, "exiting main sensor thread");
             }
