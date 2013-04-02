a61a89c923b7f95f305463afbb1de2c5510fc096
lovemrdeng <lovemrdeng@gmail.com>
2013-04-02 16:58:39 +0800

    Custom Carrier Label
    Change-Id: I5ada4ef886b7aa9f936012d3510082e2bc349a43

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3144,6 +3144,13 @@ public final class Settings {
         };
 
         /**
+         * Holds the text for the Carrier label. An empty string
+         * will bring the default text back.
+         * @hide
+         */
+        public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+ 
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
