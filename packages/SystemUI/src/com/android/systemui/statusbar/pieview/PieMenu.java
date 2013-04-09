afc53b48091da8f8a0994e9be55257ddb2f0c6ed
martincz <martincz@mokeetek.com>
2013-04-10 03:00:09 +0800

    PIE: Default Change (2/2)
    Change-Id: I02a14231f8f044b096bf780499d8eff3198d7a9f

--- packages/SystemUI/src/com/android/systemui/statusbar/pieview/PieMenu.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/pieview/PieMenu.java
@@ -284,8 +284,7 @@ public class PieMenu extends FrameLayout {
         mUseMenuAlways = Settings.System.getInt(mContext.getContentResolver(), Settings.System.PIE_MENU, 1) == 1;
         mUseSearch = Settings.System.getInt(mContext.getContentResolver(), Settings.System.PIE_SEARCH, 1) == 1;
         mUseLastApp = Settings.System.getInt(mContext.getContentResolver(), Settings.System.PIE_LAST_APP, 0) == 1;
-        mStatusMode = Settings.System.getInt(mContext.getContentResolver(),
-                Settings.System.PIE_MODE, 2);
+        mStatusMode = Settings.System.getInt(mContext.getContentResolver(), Settings.System.PIE_MODE, 0);
         mNavbarOff = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.EXPANDED_DESKTOP_STATE, 0) == 1 && !sbexpanded;
         mPieSize = Settings.System.getFloat(mContext.getContentResolver(),
