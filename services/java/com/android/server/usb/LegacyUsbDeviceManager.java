92ad2de6e523e02cba1ca8ddb96b883a91a71df8
martincz <martincz@mokeetek.com>
2013-06-26 19:36:50 +0800

    merge framework for DEFY
    Change-Id: I5c55fcf473cdad07a79f394e3386df0d765c150d

--- services/java/com/android/server/usb/LegacyUsbDeviceManager.java
+++ services/java/com/android/server/usb/LegacyUsbDeviceManager.java
@@ -107,6 +107,7 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
     private boolean mUseUsbNotification;
     private boolean mAdbEnabled;
     private boolean mLegacy = false;
+    private boolean mHasUsbService = false;
     private UsbDebuggingManager mDebuggingManager;
 
     private class AdbSettingsObserver extends ContentObserver {
@@ -117,6 +118,7 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
         public void onChange(boolean selfChange) {
             boolean enable = (Settings.Secure.getInt(mContentResolver,
                     Settings.Secure.ADB_ENABLED, 0) > 0);
+            SystemProperties.set("persist.service.adb.enable", enable ? "1" : "0");
             mHandler.sendMessage(MSG_ENABLE_ADB, enable);
         }
     }
@@ -134,12 +136,12 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
             String state = event.get("SWITCH_STATE");
 
             if (name != null && state != null) {
-                if (mLegacy) {
+                if (mLegacy && !mHasUsbService) {
                     if ("usb_mass_storage".equals(name)) {
                         mConnected  = "online".equals(state);
                         mConfigured = "online".equals(state);
                     }
-                } else {
+                } else if (!mHasUsbService) {
                     if ("usb_connected".equals(name))
                         mConnected = "1".equals(state);
                     else if ("usb_configuration".equals(name))
@@ -271,9 +273,36 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
             }
         };
 
+        final BroadcastReceiver mUsbReconfiguredReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                final String state;
+
+                if (intent.getBooleanExtra("connected", false)) {
+                    if (intent.getBooleanExtra("configured", false)) {
+                        state = "CONFIGURED";
+                    } else {
+                        state = "CONNECTED";
+                    }
+                } else {
+                    state = "DISCONNECTED";
+                }
+                if (intent.hasExtra("functions")) {
+                    mCurrentFunctions = intent.getStringExtra("functions");
+                }
+
+                if (DEBUG) {
+                    Slog.d(TAG, "Got USB reconfiguration event, state = " +
+                            state + ", functions = " + mCurrentFunctions);
+                }
+                updateState(state);
+            }
+        };
+
         public LegacyUsbHandler(Looper looper) {
             super(looper);
             char[] buffer = new char[1024];
+
+            mHasUsbService = SystemProperties.getInt("ro.usb.use_custom_service", 0) != 0;
 
             try {
                 // persist.sys.usb.config should never be unset.  But if it is, set it to "adb"
@@ -347,7 +376,10 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
                      });
 
                 // Watch for USB configuration changes
-                if (mLegacy) {
+                if (mHasUsbService) {
+                    mContext.registerReceiver(mUsbReconfiguredReceiver,
+                            new IntentFilter("com.android.internal.usb.reconfigured"));
+                } else if (mLegacy) {
                     mUEventObserver.startObserving(USB_LEGACY_MATCH);
                 } else {
                     mUEventObserver.startObserving(USB_CONNECTED_MATCH);
@@ -451,7 +483,15 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
         }
 
         private void setEnabledFunctions(String functions, boolean makeDefault) {
-            if (functions != null && makeDefault) {
+            if (mHasUsbService) {
+                if (mBootCompleted) {
+                    Intent i = new Intent("com.android.internal.usb.request_reconfigure");
+                    i.putExtra("functions", functions);
+                    i.putExtra("permanent", makeDefault);
+                    i.putExtra("enable_adb", mAdbEnabled);
+                    mContext.sendBroadcast(i);
+                }
+            } else if (functions != null && makeDefault) {
                 if (mAdbEnabled) {
                     functions = addFunction(functions, UsbManager.USB_FUNCTION_ADB);
                 } else {
@@ -679,6 +719,12 @@ public class LegacyUsbDeviceManager extends UsbDeviceManager {
                 pw.println("IOException: " + e);
             }
         }
+    }
+
+    @Override
+    public void setCurrentFunctions(String function, boolean makeDefault) {
+        if (DEBUG) Slog.d(TAG, "setCurrentFunction(" + function + ") default: " + makeDefault);
+        mHandler.sendMessage(MSG_SET_CURRENT_FUNCTION, function, makeDefault);
     }
 
     public void allowUsbDebugging(boolean alwaysAllow, String publicKey) {
