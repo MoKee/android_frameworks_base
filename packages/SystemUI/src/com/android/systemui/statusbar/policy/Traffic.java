9b92f98a189530a87727c3621cc9814e18287e47
wzhy90 <wzhy90@gmail.com>
2013-08-13 17:28:41 +0800

    New traffic speed view

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/Traffic.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/Traffic.java
@@ -25,149 +25,125 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.database.ContentObserver;
 import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.TrafficStats;
 import android.os.Handler;
 import android.os.Message;
 import android.os.UserHandle;
 import android.provider.Settings;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.TextView;
 
 public class Traffic extends TextView {
 	private boolean mAttached;
-	TrafficStats mTrafficStats;
-	boolean showTraffic;
-	Handler mHandler;
-	Handler mTrafficHandler;
-	float speed;
-	float totalRxBytes;
+	private BroadcastReceiver mConnectivityReceiver = new BroadcastReceiver() {
+		public void onReceive(Context context, Intent intent) {
+			NetworkInfo netInfo = ((android.net.ConnectivityManager)Traffic.this.mContext.getSystemService("connectivity")).getActiveNetworkInfo();
+			if ((netInfo != null) && (netInfo.isConnected()))
+				mIsNetworkConnected = true;
+			else
+				mIsNetworkConnected = false;
+			Traffic.this.updateNetworkSpeed();
+		}
+	};
+	private Context mContext;
+	private boolean mDisabled = true;
+	private Handler mHandler;
+	private boolean mIsNetworkConnected;
+	private boolean mIsRequestHideByKeyguard;
+	private long mLastTime;
+	private SettingsObserver mNetworkSpeedObserver;
+	private Runnable mNetworkSpeedRunnable = new Runnable() {
+		public void run() {
+			Traffic.this.updateNetworkSpeed();
+		}
+	};
+	private int mNetworkUpdateInterval;
+	private long mTotalBytes;
+
+	public Traffic(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		this.mContext = context;
+	}
+
+	private static String formatSpeed(Context context, long number) {
+		String unit = "K/s";
+		float speedValue = (float)number / 1024.0F;
+		if (speedValue > 999.0F) {
+			unit = "M/s";
+			speedValue /= 1024.0F;
+		}
+		Object fullString[] = new Object[2];
+		fullString[0] = Float.valueOf(speedValue);
+		fullString[1] = unit;
+		if (speedValue < 1.0F)
+			return String.format("%.2f%s", fullString);
+		if (speedValue < 10.0F)
+			return String.format("%.1f%s", fullString);
+		return String.format("%.0f%s", fullString);
+	}
+
+	private void updateNetworkSpeed() {
+		if ((this.mDisabled) || (!this.mIsNetworkConnected) || (this.mIsRequestHideByKeyguard)) {
+			setVisibility(8);
+			this.mLastTime = 0L;
+			this.mTotalBytes = 0L;
+			return;
+		}
+		long lastTime = System.currentTimeMillis();
+		long totalBytes = TrafficStats.getTotalRxBytes() + TrafficStats.getTotalTxBytes();
+		long speedValue = 0L;
+		if ((mLastTime != 0L) && (lastTime > mLastTime) && (mTotalBytes != 0L) && (totalBytes != 0L) && (totalBytes > mTotalBytes))
+			speedValue = 1000L * (totalBytes - mTotalBytes) / (lastTime - mLastTime);
+		setText(formatSpeed(mContext, speedValue));
+		setVisibility(0);
+		mLastTime = lastTime;
+		mTotalBytes = totalBytes;
+		removeCallbacks(mNetworkSpeedRunnable);
+		postDelayed(mNetworkSpeedRunnable, mNetworkUpdateInterval);
+	}
+
+	protected void onAttachedToWindow() {
+		super.onAttachedToWindow();
+		if (!this.mAttached) {
+			mAttached = true;
+			mHandler = new Handler();
+			mNetworkSpeedObserver = new SettingsObserver(mHandler);
+			mContext.registerReceiver(mConnectivityReceiver, new IntentFilter("android.net.conn.CONNECTIVITY_CHANGE"));
+			ContentResolver resolver = mContext.getContentResolver();
+			resolver.registerContentObserver(android.provider.Settings.System.getUriFor("status_bar_show_network_speed"), true, mNetworkSpeedObserver);
+			resolver.registerContentObserver(android.provider.Settings.System.getUriFor("status_bar_network_speed_interval"), true, mNetworkSpeedObserver);
+			mNetworkSpeedObserver.onChange(true);
+			setWidth((int)Math.ceil(getPaint().measureText("0.00K/s")));
+		}
+	}
+
+	protected void onDetachedFromWindow() {
+		super.onDetachedFromWindow();
+		if (this.mAttached) {
+			this.mAttached = false;
+			this.mContext.unregisterReceiver(this.mConnectivityReceiver);
+			this.mContext.getContentResolver().unregisterContentObserver(this.mNetworkSpeedObserver);
+			removeCallbacks(this.mNetworkSpeedRunnable);
+		}
+	}
 
 	class SettingsObserver extends ContentObserver {
 		SettingsObserver(Handler handler) {
 			super(handler);
 		}
 
-		void observe() {
-			ContentResolver resolver = mContext.getContentResolver();
-			resolver.registerContentObserver(Settings.System
-					.getUriFor(Settings.System.STATUS_BAR_TRAFFIC), false, this);
-		}
-
-		@Override
 		public void onChange(boolean selfChange) {
-			updateSettings();
-		}
-	}
-
-	public Traffic(Context context) {
-		this(context, null);
-	}
-
-	public Traffic(Context context, AttributeSet attrs) {
-		this(context, attrs, 0);
-	}
-
-	public Traffic(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		mHandler = new Handler();
-		SettingsObserver settingsObserver = new SettingsObserver(mHandler);
-		mTrafficStats = new TrafficStats();
-		settingsObserver.observe();
-		updateSettings();
-	}
-
-	@Override
-	protected void onAttachedToWindow() {
-		super.onAttachedToWindow();
-
-		if (!mAttached) {
-			mAttached = true;
-			IntentFilter filter = new IntentFilter();
-			filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
-			getContext().registerReceiver(mIntentReceiver, filter, null,
-					getHandler());
-		}
-		updateSettings();
-	}
-
-	@Override
-	protected void onDetachedFromWindow() {
-		super.onDetachedFromWindow();
-		if (mAttached) {
-			getContext().unregisterReceiver(mIntentReceiver);
-			mAttached = false;
-		}
-	}
-
-	private final BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
-		@Override
-		public void onReceive(Context context, Intent intent) {
-			String action = intent.getAction();
-			if (action.equals(ConnectivityManager.CONNECTIVITY_ACTION)) {
-				updateSettings();
-			}
-		}
-	};
-
-	public void updateTraffic() {
-		mTrafficHandler = new Handler() {
-			@Override
-			public void handleMessage(Message msg) {
-				speed = (mTrafficStats.getTotalRxBytes() - totalRxBytes) / 1024 / 3;
-				totalRxBytes = mTrafficStats.getTotalRxBytes();
-				DecimalFormat DecimalFormatfnum = new DecimalFormat("##0.00");
-				if (speed / 1024 >= 1) {
-					setText(DecimalFormatfnum.format(speed / 1024) + "MB/s");
-				} else if (speed <= 0.01) {
-					setText(DecimalFormatfnum.format(speed * 1024) + "B/s");
-				} else {
-					setText(DecimalFormatfnum.format(speed) + "KB/s");
-				}
-				update();
-				super.handleMessage(msg);
-			}
-		};
-		totalRxBytes = mTrafficStats.getTotalRxBytes();
-		mTrafficHandler.sendEmptyMessage(0);
-	}
-
-	private boolean getConnectAvailable() {
-		try {
-			ConnectivityManager connectivityManager = (ConnectivityManager) mContext
-					.getSystemService(Context.CONNECTIVITY_SERVICE);
-			if (connectivityManager.getActiveNetworkInfo().isConnected())
-				return true;
+			ContentResolver resolver = Traffic.this.mContext.getContentResolver();
+			if (android.provider.Settings.System.getInt(resolver, "status_bar_network_speed_interval", 4000) == 0)
+				mDisabled = true;
 			else
-				return false;
-		} catch (Exception ex) {
+				mDisabled = false;
+			mNetworkUpdateInterval = Settings.System.getInt(resolver, "status_bar_network_speed_interval", 4000);
+			Traffic.this.updateNetworkSpeed();
 		}
-		return false;
-	}
-
-	public void update() {
-		mTrafficHandler.removeCallbacks(mRunnable);
-		mTrafficHandler.postDelayed(mRunnable, 3000);
-	}
-
-	Runnable mRunnable = new Runnable() {
-		@Override
-		public void run() {
-			mTrafficHandler.sendEmptyMessage(0);
-		}
-	};
-
-	private void updateSettings() {
-		ContentResolver resolver = mContext.getContentResolver();
-		showTraffic = (Settings.System.getIntForUser(resolver,
-				Settings.System.STATUS_BAR_TRAFFIC, 0, UserHandle.USER_CURRENT) == 1);
-		if (showTraffic && getConnectAvailable()) {
-			if (mAttached) {
-				updateTraffic();
-			}
-			setVisibility(View.VISIBLE);
-		} else
-			setVisibility(View.GONE);
 	}
 }
 
