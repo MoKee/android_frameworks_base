8ecc09caf44b774312d2f52dc7d9f36ed557488c
martincz <martincz@mokeetek.com>
2013-08-20 22:29:43 +0800

    HALO: fancy flip
    Change-Id: I907eba4046dd87a3b4c01d9681fb769c5c8fb3a5

--- packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/halo/Halo.java
@@ -1181,6 +1181,11 @@ public class Halo extends FrameLayout implements Ticker.TickerCallback {
 
         CustomObjectAnimator contentYAnimator = new CustomObjectAnimator(this);
 
+        CustomObjectAnimator contentFlipAnimator = new CustomObjectAnimator(this);        
+
+            contentFlipAnimator.animate(ObjectAnimator.ofFloat(mHaloTickerWrapper, "rotationY",
+                    mTickerLeft ? -180 : 180, 0).setDuration(duration), new DecelerateInterpolator(), null, delay, null);
+
         int tickerX, tickerY;
 
         @Override        
