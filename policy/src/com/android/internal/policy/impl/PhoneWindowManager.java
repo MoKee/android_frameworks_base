cebd5915f5f9053a3de2e899f3ea1556c8b7e7bd
xplodwild <xplod@ouverta.fr>
2013-09-03 20:55:56 +0200

    Multi-window support
    Change-Id: Ie5860c2105ad9c971157b41d65f8fab337461ef6

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -3431,6 +3431,10 @@ public class PhoneWindowManager implements WindowManagerPolicy {
         }
     }
 
+    public Rect getContentRect() {
+        return new Rect(mContentLeft, mContentTop, mContentRight, mContentBottom);
+    }
+
     /** {@inheritDoc} */
     @Override
     public void layoutWindowLw(WindowState win, WindowManager.LayoutParams attrs,
