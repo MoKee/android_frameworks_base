0285bda7a14b92da39314bd810ecbb0db31e2388
martincz <martincz@mokeetek.com>
2013-09-12 23:59:40 +0800

    HALO: Mods (1/2)
    Change-Id: I55b2d6e9ac98fd01a3b850588035f45410d2887b

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2618,6 +2618,13 @@ public final class Settings {
         public static final String INACCURATE_PROXIMITY_WORKAROUND = "inaccurate_proximity_sensor";
 
         /**
+         * Holds the text for the Carrier label. An empty string
+         * will bring the default text back.
+         * @hide
+         */
+        public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+        /**
          * Receive incoming SIP calls?
          * 0 = no
          * 1 = yes
@@ -2747,13 +2754,6 @@ public final class Settings {
         public static final String QUICK_SETTINGS_RIBBON_TILES = "quick_settings_ribbon_tiles";
 
         /**
-         * Holds the text for the Carrier label. An empty string
-         * will bring the default text back.
-         * @hide
-         */
-        public static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
-
-        /**
          * Navigation controls to Use
          *
          * @hide
@@ -2787,7 +2787,7 @@ public final class Settings {
 
         /**
          * HALO reversed?, should default to 1 (yes, reverse)
-        * @hide
+         * @hide
          */
         public static final String HALO_REVERSED = "halo_reversed";
 
@@ -2796,6 +2796,36 @@ public final class Settings {
          * @hide
          */
         public static final String HALO_HIDE = "halo_hide";
+
+        /**
+         * HALO ninja?, should default to 0 (no, do not disappear when empty)
+         * @hide
+         */
+        public static final String HALO_NINJA = "halo_ninja";
+
+        /**
+         * HALO message box?, should default to 1 (yes, show message box on incoming notification)
+         * @hide
+         */
+        public static final String HALO_MSGBOX = "halo_msgbox";
+
+        /**
+         * HALO notificatoin count?, should default to 4 (both)
+         * @hide
+         */
+        public static final String HALO_NOTIFY_COUNT = "halo_notify_count";
+
+        /**
+         * HALO message box animation?, should default to 2 (flip animation)
+         * @hide
+         */
+        public static final String HALO_MSGBOX_ANIMATION = "halo_msgbox_animation";
+
+        /**
+         * HALO unlock ping?, should default to 0 (no, do not ping on unlock)
+         * @hide
+         */
+        public static final String HALO_UNLOCK_PING = "halo_unlock_ping";
 
         /**
          * HALO size fraction, default is 1.0f (normal)
