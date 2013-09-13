e2a703d3decfb347e82d4b54616fe9616142ec0c
Steve Spear <stephen.k.spear@gmail.com>
2013-09-14 00:47:43 +0200

    Frameworks: Custom System Animations ported to MoKee Open Source. Original work from Steve K. Spear AOKP (1/2)
    Change-Id: I13890cc471717882bdaaf8e4320f2b020dd0f3e6

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3597,7 +3597,27 @@ public final class Settings {
          * 6 == anticipate_overshoot_interpolator
          * 7 == bounce_interpolator
          */
-        public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator"; 
+        public static final String LISTVIEW_INTERPOLATOR = "listview_interpolator";
+
+        /**
+         * Custom system animations
+         */
+
+        public static final String[] ACTIVITY_ANIMATION_CONTROLS = new String[] {
+                "activity_open",
+                "activity_close",
+                "task_open",
+                "task_close",
+                "task_to_front",
+                "task_to_back",
+                "wallpaper_open",
+                "wallpaper_close",
+                "wallpaper_intra_open",
+                "wallpaper_intra_close",
+        };
+
+        public static final String ANIMATION_CONTROLS_DURATION = "animation_controls_duration";
+        public static final String ANIMATION_CONTROLS_NO_OVERRIDE = "animation_controls_no_override";
 
         /**
          * Volume key controls ringtone or media sound stream
