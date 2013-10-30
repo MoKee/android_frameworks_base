58a871654fd475d32914a76cd7268288857da50c
piscesmk2013 <piscesmk2013@gmail.com>
2013-10-30 23:43:25 +0800

    (4.3 new)merge frameworks/base for Defy
    Change-Id: I2ec1d788552071d0337f09383da33244be31eb8c

--- media/java/android/media/AudioService.java
+++ media/java/android/media/AudioService.java
@@ -208,6 +208,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
     // Maximum volume adjust steps allowed in a single batch call.
     private static final int MAX_BATCH_VOLUME_ADJUST_STEPS = 4;
 
+    private static final String ACTION_FM_STATE_CHANGED = "com.android.media.intent.action.FM_STATE_CHANGED";
+
     /* Sound effect file names  */
     private static final String SOUND_EFFECTS_PATH = "/media/audio/ui/";
     private static final List<String> SOUND_EFFECT_FILES = new ArrayList<String>();
@@ -228,7 +230,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
         15, // STREAM_BLUETOOTH_SCO
         7,  // STREAM_SYSTEM_ENFORCED
         15, // STREAM_DTMF
-        15  // STREAM_TTS
+        15, // STREAM_TTS
+        15  // STREAM_FM
     };
     /* mStreamVolumeAlias[] indicates for each stream if it uses the volume settings
      * of another stream: This avoids multiplying the volume settings for hidden
@@ -248,7 +251,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
         AudioSystem.STREAM_BLUETOOTH_SCO,   // STREAM_BLUETOOTH_SCO
         AudioSystem.STREAM_RING,            // STREAM_SYSTEM_ENFORCED
         AudioSystem.STREAM_RING,            // STREAM_DTMF
-        AudioSystem.STREAM_MUSIC            // STREAM_TTS
+        AudioSystem.STREAM_MUSIC,           // STREAM_TTS
+		AudioSystem.STREAM_FM               // STREAM_FM
     };
     private final int[] STREAM_VOLUME_ALIAS_NON_VOICE = new int[] {
         AudioSystem.STREAM_VOICE_CALL,      // STREAM_VOICE_CALL
@@ -260,7 +264,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
         AudioSystem.STREAM_BLUETOOTH_SCO,   // STREAM_BLUETOOTH_SCO
         AudioSystem.STREAM_MUSIC,           // STREAM_SYSTEM_ENFORCED
         AudioSystem.STREAM_MUSIC,           // STREAM_DTMF
-        AudioSystem.STREAM_MUSIC            // STREAM_TTS
+        AudioSystem.STREAM_MUSIC,           // STREAM_TTS
+		AudioSystem.STREAM_FM               // STREAM_FM
     };
     private int[] mStreamVolumeAlias;
 
@@ -277,7 +282,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
             "STREAM_BLUETOOTH_SCO",
             "STREAM_SYSTEM_ENFORCED",
             "STREAM_DTMF",
-            "STREAM_TTS"
+            "STREAM_TTS",
+	    	"STREAM_FM"
     };
 
     private boolean mLinkNotificationWithVolume;
@@ -342,6 +348,7 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
 
     // Devices currently connected
     private final HashMap <Integer, String> mConnectedDevices = new HashMap <Integer, String>();
+    private boolean mFmActive = false;
 
     // Forced device usage for communications
     private int mForcedUseForComm;
@@ -550,6 +557,7 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
         intentFilter.addAction(Intent.ACTION_USER_SWITCHED);
         intentFilter.addAction(Intent.ACTION_WIFI_DISPLAY_AUDIO);
         intentFilter.addAction(Intent.ACTION_HEADSET_PLUG);
+		intentFilter.addAction(ACTION_FM_STATE_CHANGED);
 
         intentFilter.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
         // TODO merge orientation and rotation
@@ -590,6 +598,10 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
         TelephonyManager tmgr = (TelephonyManager)
                 context.getSystemService(Context.TELEPHONY_SERVICE);
         tmgr.listen(mPhoneStateListener, PhoneStateListener.LISTEN_CALL_STATE);
+
+        if (context.getResources().getBoolean(com.android.internal.R.bool.config_fmSeparateVolumeControl)) {
+            STREAM_VOLUME_ALIAS[AudioSystem.STREAM_FM] = AudioSystem.STREAM_FM;
+        }
 
         mUseMasterVolume = context.getResources().getBoolean(
                 com.android.internal.R.bool.config_useMasterVolume);
@@ -878,6 +890,10 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
             flags &= ~AudioManager.FLAG_PLAY_SOUND;
         }
 
+        if (streamType == AudioSystem.STREAM_FM) {
+            flags &= ~AudioManager.FLAG_PLAY_SOUND;
+        }
+
         if (streamType == STREAM_REMOTE_MUSIC) {
             // don't play sounds for remote
             flags &= ~(AudioManager.FLAG_PLAY_SOUND|AudioManager.FLAG_FIXED_VOLUME);
@@ -897,6 +913,11 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
 
         ensureValidDirection(direction);
         ensureValidStreamType(streamType);
+
+        if (streamType == AudioManager.STREAM_FM && !mFmActive) {
+            Log.d(TAG, "Got request to adjust inactive FM stream, ignoring.");
+            return;
+        }
 
         // use stream type alias here so that streams with same alias have the same behavior,
         // including with regard to silent mode control (e.g the use of STREAM_RING below and in
@@ -1581,6 +1602,11 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
             return mCb;
         }
     }
+
+    /** @see AudioManager#isFmActive() */
+    public boolean isFmActive() {
+            return mFmActive;
+        }
 
     /** @see AudioManager#setMode(int) */
     public void setMode(int mode, IBinder cb) {
@@ -2660,6 +2686,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                     if (DEBUG_VOL)
                         Log.v(TAG, "getActiveStreamType: Forcing STREAM_MUSIC stream active");
                     return AudioSystem.STREAM_MUSIC;
+                } else if (mFmActive) {
+                    return AudioSystem.STREAM_FM;
                 } else {
                     if (mVolumeKeysControlRingStream) {
                         if (DEBUG_VOL)
@@ -4254,6 +4282,8 @@ public class AudioService extends IAudioService.Stub implements OnFinished {
                         null,
                         SAFE_VOLUME_CONFIGURE_TIMEOUT_MS);
                 adjustCurrentStreamVolume();
+            } else if (action.equals(ACTION_FM_STATE_CHANGED)) {
+                mFmActive = intent.getBooleanExtra("active", false);
             } else if (action.equals(Intent.ACTION_PACKAGE_REMOVED)
                     || action.equals(Intent.ACTION_PACKAGE_DATA_CLEARED)) {
                 if (!intent.getBooleanExtra(Intent.EXTRA_REPLACING, false)) {
