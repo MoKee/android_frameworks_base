e49bb957f6e67c0f5b4b787838dbf43afbc07420
wszfer <wszfer@gmail.com>
2013-11-16 19:48:54 +0800

    Screenshot of the default display in the power key menu
    Change-Id: I94d958653a9b65897ef85312e1b2b36a16773ea4

--- policy/src/com/android/internal/policy/impl/GlobalActions.java
+++ policy/src/com/android/internal/policy/impl/GlobalActions.java
@@ -347,7 +347,7 @@ class GlobalActions implements DialogInterface.OnDismissListener, DialogInterfac
         // next: screenshot
         // only shown if enabled, disabled by default
         boolean showScreenshot = Settings.System.getIntForUser(cr,
-                Settings.System.POWER_MENU_SCREENSHOT_ENABLED, 0, UserHandle.USER_CURRENT) == 1;
+                Settings.System.POWER_MENU_SCREENSHOT_ENABLED, 1, UserHandle.USER_CURRENT) == 1;
         if (showScreenshot) {
             mItems.add(
                 new SinglePressAction(R.drawable.ic_lock_screenshot, R.string.global_action_screenshot) {
