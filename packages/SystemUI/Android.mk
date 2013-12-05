be64d6ba9c298c3776211fe9206743e106ba48a1
martincz <martincz@mokeetek.com>
2013-12-05 08:01:20 +0800

    split systemui
    Change-Id: If2dbe3338ea02ee6d16f873787771f1c4150e3a5

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -3,8 +3,13 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 
+LOCAL_AAPT_INCLUDE_ALL_RESOURCES := true
+LOCAL_AAPT_FLAGS += --auto-add-overlay
+
 LOCAL_SRC_FILES := $(call all-java-files-under, src) \
-    src/com/android/systemui/EventLogTags.logtags
+    src/com/android/systemui/EventLogTags.logtags $(call all-java-files-under,../MoKeeExt/src)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res $(LOCAL_PATH)/../MoKeeExt/res
 
 LOCAL_JAVA_LIBRARIES := telephony-common
 
