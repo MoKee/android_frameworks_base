87cc5445a2ea52cd845edcd3f1c2d16795ae443d
David96 <xd_e-mail@gmx.de>
2014-01-01 18:31:35 +0100

    Lockscreen Notifications: make width dynamic (WRAP_CONTENT)

--- packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationHostView.java
@@ -127,7 +127,7 @@ public class NotificationHostView extends FrameLayout {
         private boolean animating = false;
         private boolean preventClick = false;
         private float initialX;
-        private float initialXPos;
+        private float delta;
         private boolean shown = false;
         private float previousX = 0;
         private float previousTime = 0;
@@ -186,7 +186,6 @@ public class NotificationHostView extends FrameLayout {
                 }
             }
         }
-
         @Override
         public boolean onInterceptTouchEvent(MotionEvent event) {
             mViewMediatorCallback.userActivity();
@@ -194,22 +193,24 @@ public class NotificationHostView extends FrameLayout {
                 View v = getChildAt(0);
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        initialX = event.getRawX();
-                        initialXPos = v.getX();
+                        initialX = event.getX();
+                        delta = initialX - v.getX();
                         preventClick = false;
                         break;
                     case MotionEvent.ACTION_MOVE:
                         float time = System.nanoTime() / 1000000.0f;
-                        float x = event.getRawX() - initialX + initialXPos;
+                        float x = (event.getX() - delta);
+                        float xr = x - (mDisplayWidth - v.getWidth());
                         speedX += (x - previousX) / (time - previousTime);
                         count++;
                         previousX = x;
                         previousTime = time;
-                        if (speedX < 0 && x < 0) {
-                            v.setAlpha(1f + (x / v.getWidth()));
+                        if (speedX < 0 && x < mDisplayWidth - v.getWidth()) {
+                            v.setAlpha(1f + (xr / (v.getWidth() * (SWIPE * 2))));
                         }
                         if (mShownNotifications == 0 || (shown && mShownNotifications == 1))
-                            NotificationHostView.this.setBackgroundColor(Color.argb(MAX_ALPHA - (int)(Math.abs(x) / v.getWidth() * MAX_ALPHA), 0, 0, 0));
+                            NotificationHostView.this.setBackgroundColor(Color.argb(MAX_ALPHA -
+                                    (int)(Math.abs(xr) / v.getWidth() * MAX_ALPHA), 0, 0, 0));
                         if (swipeGesture  || Math.abs(event.getX() - initialX) > CLICK_THRESHOLD) {
                             swipeGesture = true;
                             preventClick = true;
@@ -220,7 +221,7 @@ public class NotificationHostView extends FrameLayout {
                         break;
                     case MotionEvent.ACTION_UP:
                         speedX /= count;
-                        if (v.getX() < -SWIPE * mDisplayWidth &&
+                        if (v.getX() - (mDisplayWidth - v.getWidth())< -SWIPE * mDisplayWidth &&
                                 (NotificationViewManager.config.dismissAll || statusBarNotification.isClearable())) {
                             removeNotification(statusBarNotification);
                         } else if (v.getX() < (SWIPE * mDisplayWidth)) {
@@ -335,7 +336,7 @@ public class NotificationHostView extends FrameLayout {
     }
 
     private void handleAddNotification(final boolean showNotification, boolean forceBigContentView) {
-        NotificationView nv = mNotificationsToAdd.poll();
+        final NotificationView nv = mNotificationsToAdd.poll();
         Log.d(TAG, "Add: " + describeNotification(nv.statusBarNotification));
         final StatusBarNotification sbn = nv.statusBarNotification;
         mDismissedNotifications.remove(describeNotification(sbn));
@@ -350,9 +351,9 @@ public class NotificationHostView extends FrameLayout {
                 (NotificationViewManager.config.expandedView || sbn.getNotification().contentView == null);
         RemoteViews rv = forceBigContentView && bigContentView ? sbn.getNotification().bigContentView : sbn.getNotification().contentView;
         final View remoteView = rv.apply(mContext, null);
-        int x = Math.round(mDisplayWidth - mNotificationMinHeight);
-        remoteView.setX(x);
         remoteView.setBackgroundColor(0x33ffffff);
+        remoteView.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        remoteView.setX(mDisplayWidth - mNotificationMinHeight);
         if (bigContentView && forceBigContentView) {
             setBackgroundRecursive((ViewGroup)remoteView);
         }
@@ -382,12 +383,18 @@ public class NotificationHostView extends FrameLayout {
             final NotificationView oldView = (NotificationView) mNotifications.get(describeNotification(sbn));
             Runnable r = new Runnable() {
                 public void run() {
-                    remoteView.setX(oldView.getChildAt(0).getX());
+                    final float oldX = oldView.getChildAt(0).getX() - (mDisplayWidth - oldView.getChildAt(0).getWidth());
                     oldView.removeAllViews();
                     oldView.addView(remoteView);
                     oldView.statusBarNotification = sbn;
-                    if (showNotification)
-                        showNotification(oldView);
+                    mHandler.post(new Runnable() {
+                        public void run() {
+                            remoteView.setX(oldX + (mDisplayWidth - remoteView.getWidth()));
+                            if(showNotification) {
+                                showNotification(oldView);
+                            }
+                        }
+                    });
                 }
             };
             if (oldView.animating) {
@@ -403,9 +410,16 @@ public class NotificationHostView extends FrameLayout {
 
         mNotifView.addView(nv);
         mNotifications.put(describeNotification(sbn), nv);
-        if(showNotification)
-            showNotification(nv);
         mNotifView.bringToFront();
+        if(showNotification) {
+            // showNotification uses v.getWidth but until the layout is done, this just returns 0.
+            // by using mHandler.post, we wait until getWidth returns the real width
+            mHandler.post(new Runnable() {
+                public void run() {
+                    showNotification(nv);
+                }
+            });
+        }
     }
 
     public void removeNotification(final StatusBarNotification sbn) {
@@ -467,10 +481,14 @@ public class NotificationHostView extends FrameLayout {
     private void showNotification(NotificationView nv) {
         if (!NotificationViewManager.config.privacyMode) {
             View v = nv.getChildAt(0);
-            int duration = getDurationFromDistance(v, 0, 0, Math.abs(nv.speedX));
+            int targetX = mDisplayWidth - v.getWidth();
+            int duration = getDurationFromDistance(v, targetX, 0, Math.abs(nv.speedX));
             v.animate().setDuration(duration).alpha(1);
-            animateTranslation(v, 0, 0, duration);
-            if (mShownNotifications == 0) animateBackgroundColor(Color.argb(MAX_ALPHA, 0, 0, 0), duration);
+            animateTranslation(v, targetX, 0, duration);
+            if (mShownNotifications == 0 ||
+                    (mShownNotifications == 1 && nv.shown)) {
+                animateBackgroundColor(Color.argb(MAX_ALPHA, 0, 0, 0), duration);
+            }
             if (!nv.shown) {
                 nv.shown = true;
                 mShownNotifications++;
