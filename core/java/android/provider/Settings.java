82ab43ad610dc0bf9028f2d61e0f6fc12cd074d8
maxwen <max.weninger@gmail.com>
2014-01-19 23:06:31 +0100

    [1/2] Frameworks: Wakelock Blocker
    Change-Id: Id4fd1d8da6ed40517094a5a156f7c6fea622e985

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3908,6 +3908,18 @@ public final class Settings {
         public static final String VOICE_LAUNCH_INTENT = "voice_launch_intent";
 
         /**
+         * Enable blocking wakelock
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_ENABLED = "wakelock_blocking_enabled";
+
+        /**
+         * List of wakelock blocks selected
+         * @hide
+         */
+        public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
