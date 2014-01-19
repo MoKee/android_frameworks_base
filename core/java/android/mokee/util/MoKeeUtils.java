5bcc24c80225c7c636cc8486b858cc0e8f66179f
wszfer <wszfer@gmail.com>
2014-01-19 19:10:34 +0800

    format
    Change-Id: Ic5b38012fa51b1349d09a356da383011350bebbd

--- core/java/android/mokee/util/MoKeeUtils.java
+++ core/java/android/mokee/util/MoKeeUtils.java
@@ -62,7 +62,7 @@ public class MoKeeUtils {
      * @param size
      * @return
      */
-    public static String formetFileSize(long size) {
+    public static String formatFileSize(long size) {
         DecimalFormat df = new DecimalFormat("#0.00");
         String fileSizeString = "";
         if (size < 1024) {
