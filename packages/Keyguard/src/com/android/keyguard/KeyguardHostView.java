b0d3f41c00651aedf7489d3f4b550d731c4626b2
martincz <martincz@mokeedev.com>
2014-06-09 19:45:35 +0800

    Lockscreen Notification: invisible clearall button when NonClearable notification
    Change-Id: Id8ec7037253d735a07a0844c2581b9c5d0834a86

--- packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
+++ packages/Keyguard/src/com/android/keyguard/KeyguardHostView.java
@@ -2,6 +2,7 @@
  * Copyright (c) 2013, The Linux Foundation. All rights reserved.
  * Not a Contribution.
  * Copyright (C) 2012 The Android Open Source Project
+ * Copyright (C) 2014 The MoKee OpenSource Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
