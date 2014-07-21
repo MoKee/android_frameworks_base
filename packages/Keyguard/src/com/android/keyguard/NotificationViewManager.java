6e34e5d9cfaa7e8ece76c853123eb4077d56e388
martincz <martincz@mokeedev.com>
2014-07-21 12:42:19 +0800

    lockscreen notification: set default pocket mode to 0
    Change-Id: I36bff59aca2e6f3564ec05d0eebba70d976311c7

--- packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
+++ packages/Keyguard/src/com/android/keyguard/NotificationViewManager.java
@@ -70,7 +70,7 @@ public class NotificationViewManager {
     class Configuration extends ContentObserver {
         //User configurable values, set defaults here
         public boolean showAlways = true;
-        public boolean pocketMode = true;
+        public boolean pocketMode = false;
         public boolean hideLowPriority = false;
         public boolean hideNonClearable = true;
         public boolean dismissAll = true;
