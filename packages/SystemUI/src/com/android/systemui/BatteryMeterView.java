b116e9016ab1b04a91e77e93517d59744cefba77
Ryuinferno <ryuinferno.xda@gmail.com>
2014-11-15 17:15:50 +0100

    base: Fix battery percentage text for DSB
    Change-Id: Iac46502f0c312efb90771ab1bdb3b641c13701c8

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -608,8 +608,6 @@ public class BatteryMeterView extends View implements DemoMode {
                                 : (tracker.level == 100 ? full : nofull)));
                 mTextHeight = -mTextPaint.getFontMetrics().ascent;
 
-                mTextPaint.setColor(doOverride ? mOverrideIconColor : 0xFF000000);
-
                 final String str = String.valueOf(SINGLE_DIGIT_PERCENT ? (level/10) : level);
                 final float x  = mWidth * 0.5f;
                 final float y = pt + (height + mTextHeight) * 0.47f;
