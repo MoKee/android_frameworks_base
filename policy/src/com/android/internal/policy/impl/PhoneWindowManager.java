8408d42c1c484afc863e13b51407d5970ae84a8f
x10forevers <android.x10f@gmail.com>
2015-01-12 04:55:06 +0800

    Revert "Framework : Add Shortcut 1/2"
    This reverts commit 868d12fe4acb7725003129c3a36cc9c17a192ded.
    
    Change-Id: I49ddc2219362e47760b5d0e7b3280b48305a1c26

--- policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
+++ policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
@@ -198,8 +198,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     private static final int KEY_ACTION_LAUNCH_CAMERA = 6;
     private static final int KEY_ACTION_SLEEP = 7;
     private static final int KEY_ACTION_LAST_APP = 8;
-    private static final int KEY_ACTION_SCREENSHOT = 9;
-    private static final int KEY_ACTION_REBOOT = 10;
 
     // Masks for checking presence of hardware keys.
     // Must match values in core/res/res/values/config.xml
@@ -1229,12 +1227,6 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                 break;
             case KEY_ACTION_LAST_APP:
                 ActionUtils.switchToLastApp(mContext, mCurrentUserId);
-                break;
-            case KEY_ACTION_SCREENSHOT:
-                mHandler.postDelayed(mScreenshotRunnable, getScreenshotChordLongPressDelay());
-                break;
-            case KEY_ACTION_REBOOT:
-                showGlobalActions();
                 break;
             default:
                 break;
