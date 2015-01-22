09839a4ba3daeead996b0e1d68f9227cea3b6f7d
martincz <martincz@mokeedev.com>
2015-01-23 02:39:38 +0800

    base: Direct call (1/4)
    Change-Id: I9b052bcc59587837771458372e2b62159a75bd3f

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3255,6 +3255,18 @@ public final class Settings {
         public static final String ANSWER_VOLUME_BUTTON_BEHAVIOR_ANSWER = "call_volume_answer";
 
         /**
+         * Put on your ear, and this will automatically call current number in dialer
+         * @hide
+         */
+        public static final String DIRECT_CALL_FOR_DIALER = "direct_call_for_dialer";
+
+        /**
+         * Put on your ear, and this will automatically call current number in message
+         * @hide
+         */
+        public static final String DIRECT_CALL_FOR_MMS = "direct_call_for_mms";
+
+        /**
          * ListView Animations
          * 0 == None
          * 1 == Wave (Left)
