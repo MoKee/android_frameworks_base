012efb14f30cb3c6bad3f7e0a652802368f34d72
martincz <martincz@mokeedev.com>
2015-01-30 04:26:54 +0800

    base: code cleanup
    Change-Id: Ic31ae738f8739d3d1c061bab039c03a977617b07

---
+++ mokee/private/phonelocation/Android.mk
@@ -0,0 +1,34 @@
+# Copyright (C) 2015 The MoKee OpenSource Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# We have to use BUILD_PREBUILT instead of PRODUCT_COPY_FIES,
+# because SMALLER_FONT_FOOTPRINT is only available in Android.mks.
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := MoKeePhoneLocation
+LOCAL_SRC_FILES := libmokee-phonelocation.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT)/lib
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := MoKeePhoneLocationData
+LOCAL_SRC_FILES := mokee-phonelocation.dat
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_PATH := $(TARGET_OUT)/media/location
+
+include $(BUILD_PREBUILT)
