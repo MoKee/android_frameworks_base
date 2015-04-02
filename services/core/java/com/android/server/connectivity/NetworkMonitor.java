ee565c325d01156821bcf574cbe6fb5a22483a32
martincz <martincz@mokeedev.com>
2015-04-02 18:57:09 +0800

    base: update default server url
    Change-Id: Ibb3f2d50cbdc8194fed20e154c38281c22da92f6

--- services/core/java/com/android/server/connectivity/NetworkMonitor.java
+++ services/core/java/com/android/server/connectivity/NetworkMonitor.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2014 The Android Open Source Project
+ * Copyright (C) 2015 The MoKee OpenSource Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -69,7 +70,7 @@ import java.util.Random;
 public class NetworkMonitor extends StateMachine {
     private static final boolean DBG = true;
     private static final String TAG = "NetworkMonitor";
-    private static final String DEFAULT_SERVER = "connectivitycheck.android.com";
+    private static final String DEFAULT_SERVER = "download.mokeedev.com";
     private static final int SOCKET_TIMEOUT_MS = 10000;
     public static final String ACTION_NETWORK_CONDITIONS_MEASURED =
             "android.net.conn.NETWORK_CONDITIONS_MEASURED";
