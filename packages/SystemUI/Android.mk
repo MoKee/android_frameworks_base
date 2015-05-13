85614bd04f42cdc27c32e3a94ea7b767083b5c60
martincz <martincz@mokeedev.com>
2015-05-13 11:02:01 +0800

    Revert "SystemUI: Squash custom tile commits"
    This reverts commit 5dea9e812eb4fd6c618c0090b78ca238aec894b2.

--- packages/SystemUI/Android.mk
+++ packages/SystemUI/Android.mk
@@ -10,8 +10,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src) \
 LOCAL_STATIC_JAVA_LIBRARIES := Keyguard \
     android-support-v7-palette \
     android-support-v4 \
-    android-visualizer \
-    org.cyanogenmod.platform.sdk
+    android-visualizer
 
 LOCAL_JAVA_LIBRARIES := telephony-common
 
