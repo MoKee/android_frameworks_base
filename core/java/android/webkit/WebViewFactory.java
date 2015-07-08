bba0cea35ff0192f6a3ae80b6ccf1e73c3c8ca27
martincz <martincz@mokeedev.com>
2015-07-09 06:36:52 +0800

    base: fix NPE
    Change-Id: Iac2c22a913f890b4291b0c3861f750559b6b6d3f

--- core/java/android/webkit/WebViewFactory.java
+++ core/java/android/webkit/WebViewFactory.java
@@ -18,6 +18,7 @@ package android.webkit;
 
 import android.annotation.SystemApi;
 import android.app.ActivityManagerInternal;
+import android.app.ActivityThread;
 import android.app.AppGlobals;
 import android.app.Application;
 import android.content.Context;
@@ -99,7 +100,7 @@ public final class WebViewFactory {
             if (sProviderInstance != null) return sProviderInstance;
 
             final int uid = android.os.Process.myUid();
-            if (uid == android.os.Process.ROOT_UID || uid == android.os.Process.SYSTEM_UID && !sPackageInfo.packageName.equals("com.android.settings")) {
+            if (uid == android.os.Process.ROOT_UID || uid == android.os.Process.SYSTEM_UID && !ActivityThread.currentPackageName().equals("com.android.settings")) {
                 throw new UnsupportedOperationException(
                         "For security reasons, WebView is not allowed in privileged processes");
             }
