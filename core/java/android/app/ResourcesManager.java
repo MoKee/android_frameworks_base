1a2479f88c7282d624871bb3dd2da4b5510c080f
martincz <martincz@mokeedev.com>
2015-07-11 12:11:09 +0800

    Revert "Account for cmsdk resources when attaching theme assets"
    This reverts commit 7c1ee7b3b53121d997df8bab1035d93856dc5053.

--- core/java/android/app/ResourcesManager.java
+++ core/java/android/app/ResourcesManager.java
@@ -69,12 +69,6 @@ public class ResourcesManager {
     Configuration mResConfiguration;
     final Configuration mTmpConfig = new Configuration();
 
-    /**
-     * Number of default assets attached to a Resource object's AssetManager
-     * This currently includes framework and cmsdk resources
-     */
-    private static final int NUM_DEFAULT_ASSETS = 2;
-
     public static ResourcesManager getInstance() {
         synchronized (ResourcesManager.class) {
             if (sResourcesManager == null) {
@@ -531,10 +525,10 @@ public class ResourcesManager {
         String basePackageName = null;
         String resourcePackageName = null;
         int count = assets.getBasePackageCount();
-        if (count > NUM_DEFAULT_ASSETS) {
-            basePackageName  = assets.getBasePackageName(NUM_DEFAULT_ASSETS);
-            resourcePackageName = assets.getBaseResourcePackageName(NUM_DEFAULT_ASSETS);
-        } else if (count == NUM_DEFAULT_ASSETS) {
+        if (count > 1) {
+            basePackageName  = assets.getBasePackageName(1);
+            resourcePackageName = assets.getBaseResourcePackageName(1);
+        } else if (count == 1) {
             basePackageName  = assets.getBasePackageName(0);
         } else {
             return false;
@@ -663,9 +657,9 @@ public class ResourcesManager {
         // first or else the system will crash!
         String basePackageName;
         int count = assets.getBasePackageCount();
-        if (count > NUM_DEFAULT_ASSETS) {
-            basePackageName  = assets.getBasePackageName(NUM_DEFAULT_ASSETS);
-        } else if (count == NUM_DEFAULT_ASSETS) {
+        if (count > 1) {
+            basePackageName  = assets.getBasePackageName(1);
+        } else if (count == 1) {
             basePackageName  = assets.getBasePackageName(0);
         } else {
             return false;
