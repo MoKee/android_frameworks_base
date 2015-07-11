6d1c8d89fca4133365c79676e88e3f35725c7feb
martincz <martincz@mokeedev.com>
2015-07-11 19:33:15 +0800

    base: code cleanup
    Change-Id: I07e59110a218bb7305625393e3130232b9878580

--- policy/Android.mk
+++ policy/Android.mk
@@ -5,7 +5,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
-            
+
 LOCAL_MODULE := android.policy
 LOCAL_STATIC_JAVA_LIBRARIES := \
     org.mokee.platform.sdk
