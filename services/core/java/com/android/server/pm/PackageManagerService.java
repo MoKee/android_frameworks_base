2e4b0bbe045bbcf0ac4ae1283d3268c6178d57fc
martincz <martincz@mokeedev.com>
2015-09-15 20:59:30 +0800

    Revert "Added Changes for Custom Resolver Activity"
    This reverts commit c0ee56724ea2eda73dcce3500664e64fa1949667.

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -91,7 +91,6 @@ import com.android.server.pm.Settings.DatabaseVersion;
 import com.android.server.storage.DeviceStorageMonitorInternal;
 import com.android.server.Watchdog;
 
-import cyanogenmod.app.suggest.AppSuggestManager;
 import org.xmlpull.v1.XmlSerializer;
 
 import android.app.ActivityManager;
@@ -3591,13 +3590,6 @@ public class PackageManagerService extends IPackageManager.Stub {
         return null;
     }
 
-    private boolean shouldIncludeResolveActivity(Intent intent) {
-        synchronized(mPackages) {
-            AppSuggestManager suggest = AppSuggestManager.getInstance(mContext);
-            return (suggest != null) ? suggest.handles(intent) : false;
-        }
-    }
-
     @Override
     public List<ResolveInfo> queryIntentActivities(Intent intent,
             String resolvedType, int flags, int userId) {
@@ -3646,9 +3638,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                 if (resolveInfo != null) {
                     result.add(resolveInfo);
                     Collections.sort(result, mResolvePrioritySorter);
-                }
-                if (result.size() == 0 && shouldIncludeResolveActivity(intent)) {
-                    result.add(mResolveInfo);
                 }
                 return result;
             }
