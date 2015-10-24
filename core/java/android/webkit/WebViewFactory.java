a685afbc01a0e3bece5de3c8267f2a365cadaff6
martincz <martincz@mokeedev.com>
2015-10-25 00:04:14 +0800

    base: add MoKeeUtils class
    Change-Id: I2689541deda3a3f178a8489337b8ada77c460f3a

--- core/java/android/webkit/WebViewFactory.java
+++ core/java/android/webkit/WebViewFactory.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2012 The Android Open Source Project
- * Copyright (C) 2015 The MoKee Open Source Project
+ * Copyright (C) 2015-2016 The MoKee Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
