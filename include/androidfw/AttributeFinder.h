de4307430683b452a905990e70a055c9e2ceea30
martincz <martincz@mokeedev.com>
2017-01-03 18:55:29 +0800

    base: fix wrong res id
    Change-Id: If9a1673a06bcdef650db0784929b3b2049af7cfc

--- include/androidfw/AttributeFinder.h
+++ include/androidfw/AttributeFinder.h
@@ -101,7 +101,7 @@ void BackTrackingAttributeFinder<Derived, Iterator>::jumpToClosestAttribute(cons
         case 0x01:
             mCurrent = mFrameworkStart;
             break;
-        case 0x0b:
+        case 0x2f:
             mCurrent = mSTFrameworkStart;
             break;
         case 0x3f:
@@ -140,7 +140,7 @@ void BackTrackingAttributeFinder<Derived, Iterator>::markCurrentPackageId(const 
         case 0x01:
             mFrameworkStart = mCurrent;
             break;
-        case 0x0b:
+        case 0x2f:
             mSTFrameworkStart = mCurrent;
             break;
         case 0x3f:
