983a27468e9be6bd3cc2a1c0cb1c356447f6b8b9
martincz <martincz@mokeedev.com>
2017-01-07 23:30:27 +0800

    Revert "Add interface for app drag file."
    This reverts commit 85bff3281bbf6a1fe69c9199783a30a24cbe7c01.
    
    Change-Id: I2e512fcdc6eeab84c1919a1bd5c590419f931c8e

--- core/java/android/view/onestep/OneStepManagerImpl.java
+++ core/java/android/view/onestep/OneStepManagerImpl.java
@@ -1,19 +1,4 @@
-/**
- * Copyright (c) 2016, The Smartisan Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
- 
+
 package android.view.onestep;
 
 import java.util.ArrayList;
@@ -148,28 +133,32 @@ public final class OneStepManagerImpl implements OneStepManager {
     @Override
     public boolean isInOneStepMode() {
         try {
-            return sInstanse.get().isInOneStepMode();
+            sInstanse.get().isInOneStepMode();
+            ;
         } catch (RemoteException e) {
             throw e.rethrowAsRuntimeException();
         }
+        return false;
     }
 
     @Override
     public int getOneStepModeState() {
         try {
-            return sInstanse.get().getOneStepModeState();
+            sInstanse.get().getOneStepModeState();
         } catch (RemoteException e) {
             throw e.rethrowAsRuntimeException();
         }
+        return 0;
     }
 
     @Override
     public boolean isFocusedOnOneStep() {
         try {
-            return sInstanse.get().isFocusedOnOneStep();
+            sInstanse.get().isFocusedOnOneStep();
         } catch (RemoteException e) {
             throw e.rethrowAsRuntimeException();
         }
+        return false;
     }
 
     @Override
