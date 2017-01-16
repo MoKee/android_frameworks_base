a228e672157e7f7829cd56cea3e4e513cfd61abc
martincz <martincz@mokeedev.com>
2017-01-16 21:58:01 +0800

    BigBang: wait for Smartisan version (2/2)
    This reverts commit 6bd1ebaee255db2b4e4d203a269eb238f05c24f8.
    
    Change-Id: I37c5a0a1c1eb7cdda6034e458bbcfd26094da4d5

--- core/java/android/content/ClipboardManager.java
+++ core/java/android/content/ClipboardManager.java
@@ -1,6 +1,5 @@
 /**
  * Copyright (c) 2010, The Android Open Source Project
- * Copyright (c) 2016, The MoKee Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -54,8 +53,6 @@ public class ClipboardManager extends android.text.ClipboardManager {
     private static IClipboard sService;
 
     private final Context mContext;
-
-    private long previousTime = 0;
 
     private final ArrayList<OnPrimaryClipChangedListener> mPrimaryClipChangedListeners
              = new ArrayList<OnPrimaryClipChangedListener>();
@@ -122,14 +119,6 @@ public class ClipboardManager extends android.text.ClipboardManager {
         try {
             if (clip != null) {
                 clip.prepareToLeaveProcess(true);
-                // Fix twice call
-                long now = System.currentTimeMillis();
-                if (now - previousTime  > 300) {
-                    previousTime = now;
-                    Intent intent = new Intent();
-                    intent.setClassName("com.mokee.bigbang", "com.mokee.bigbang.service.ListenClipboardService");
-                    mContext.startService(intent);
-                }
             }
             getService().setPrimaryClip(clip, mContext.getOpPackageName());
         } catch (RemoteException e) {
@@ -245,6 +234,5 @@ public class ClipboardManager extends android.text.ClipboardManager {
         for (int i=0; i<listeners.length; i++) {
             ((OnPrimaryClipChangedListener)listeners[i]).onPrimaryClipChanged();
         }
-
     }
 }
