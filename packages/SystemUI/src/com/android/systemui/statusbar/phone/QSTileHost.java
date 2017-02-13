9c3097bcbe2b94cb75d9135cea27ad2a4ffeee46
Roman Birg <roman@cyngn.com>
2017-02-13 23:31:28 +0100

    Add Compass tile
    YAOSP-NG : port to 7.1.1
    
    Change-Id: I64e7ea62e1dd7584fd45bfabfde8364ab82a4c51
    Signed-off-by: Jean-Pierre Rasquin <yank555.lu@gmail.com>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/QSTileHost.java
@@ -46,6 +46,7 @@ import com.android.systemui.qs.tiles.BluetoothTile;
 import com.android.systemui.qs.tiles.CastTile;
 import com.android.systemui.qs.tiles.CellularTile;
 import com.android.systemui.qs.tiles.ColorInversionTile;
+import com.android.systemui.qs.tiles.CompassTile;
 import com.android.systemui.qs.tiles.DataSaverTile;
 import com.android.systemui.qs.tiles.DndTile;
 import com.android.systemui.qs.tiles.FlashlightTile;
@@ -444,6 +445,7 @@ public class QSTileHost implements QSTile.Host, Tunable {
         else if (tileSpec.equals("saver")) return new DataSaverTile(this);
         else if (tileSpec.equals("night")) return new NightDisplayTile(this);
         else if (tileSpec.equals("nfc")) return new NfcTile(this);
+        else if (tileSpec.equals("compass")) return new CompassTile(this);
         else if (tileSpec.equals("screenshot")) return new ScreenShotTile(this);
         // Intent tiles.
         else if (tileSpec.startsWith(IntentTile.PREFIX)) return IntentTile.create(this,tileSpec);
