94d862cc6a4d5b26c2a51e9761a664d0efc97c21
martincz <martincz@mokeedev.com>
2017-08-24 05:00:36 +0800

    base: mokee cloud interface
    Change-Id: I70128a7226bac1e7434436df69e687872ddc042e

---
+++ mokee/java/android/mokee/utils/MoKeeUtils.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2012-2018 The MoKee Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package android.mokee.utils;
+
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import java.text.DecimalFormat;
+import java.util.Locale;
+
+public class MoKeeUtils {
+
+    public static boolean isSupportLanguage(boolean excludeTW) {
+        Locale locale = Resources.getSystem().getConfiguration().locale;
+        if (excludeTW) {
+            return locale.getLanguage().startsWith(Locale.CHINESE.getLanguage())
+                    && !locale.getCountry().equals("TW");
+        } else {
+            return locale.getLanguage().startsWith(Locale.CHINESE.getLanguage());
+        }
+    }
+
+    public static boolean isOnline(Context context) {
+        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = connectivityManager.getActiveNetworkInfo();
+        if (netInfo != null && netInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isApkInstalledAndEnabled(String packagename, Context context) {
+        int state;
+        try {
+            context.getPackageManager().getPackageInfo(packagename, 0);
+            state = context.getPackageManager().getApplicationEnabledSetting(packagename);
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+        return state != PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+                && state != PackageManager.COMPONENT_ENABLED_STATE_DISABLED_USER ? true : false;
+    }
+
+    public static boolean isApkInstalled(String packagename, Context context) {
+        try {
+            context.getPackageManager().getPackageInfo(packagename, 0);
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isSystemApp(String packagename, Context context) {
+        PackageInfo packageInfo;
+        try {
+            packageInfo = context.getPackageManager().getPackageInfo(packagename, 0);
+        } catch (NameNotFoundException e) {
+            return false;
+        }
+        return ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) != 0);
+    }
+
+}
