dbedbd87ac36841ed67f3b2ff404ef5075cdae09
chengzhi.hou <hcz017@gmail.com>
2017-09-12 01:24:34 +0800

    QS: Add set default data dub in DataUsageDetailView
    Change-Id: I7704db9c1a8d0025c7e23e0630470feaf0641763

--- packages/SystemUI/src/com/android/systemui/qs/tiles/DataUsageDetailView.java
+++ packages/SystemUI/src/com/android/systemui/qs/tiles/DataUsageDetailView.java
@@ -20,9 +20,14 @@ import android.annotation.ColorInt;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.telephony.SubscriptionInfo;
+import android.telephony.SubscriptionManager;
+import android.telephony.TelephonyManager;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.android.settingslib.Utils;
@@ -32,6 +37,7 @@ import com.android.systemui.R;
 import com.android.systemui.qs.DataUsageGraph;
 
 import java.text.DecimalFormat;
+import java.util.List;
 
 /**
  * Layout for the data usage detail in quick settings.
@@ -44,8 +50,12 @@ public class DataUsageDetailView extends LinearLayout {
 
     private final DecimalFormat FORMAT = new DecimalFormat("#.##");
 
+    private Context mContext;
+    private RadioGroup mRadioGroup;
+
     public DataUsageDetailView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mContext = context;
     }
 
     @Override
@@ -121,6 +131,57 @@ public class DataUsageDetailView extends LinearLayout {
             infoTop.setVisibility(View.GONE);
         }
 
+        // prepare view
+        mRadioGroup = (RadioGroup) findViewById(R.id.sim_cards);
+        mRadioGroup.removeAllViews();
+        int activeSimCount = getActiveSimCount();
+        if (activeSimCount > 1) {
+            for (int slotId = 0; slotId < activeSimCount; slotId++) {
+                String carrierName = getCarrierName(slotId);
+                RadioButton radioButton = new RadioButton(mContext);
+                radioButton.setText(mContext.getString(R.string.use_data, carrierName));
+                mRadioGroup.addView(radioButton, LayoutParams.MATCH_PARENT,
+                        LayoutParams.WRAP_CONTENT);
+            }
+
+            // check default data slot
+            int defaultDataSubId = SubscriptionManager.getDefaultDataSubId();
+            int defaultDataSlotId = SubscriptionManager.getSlotId(defaultDataSubId);
+            RadioButton radioButton = (RadioButton) mRadioGroup.getChildAt(defaultDataSlotId);
+            radioButton.setChecked(true);
+
+            // set checkListener
+            mRadioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(RadioGroup group, int checkedId) {
+                    for (int slotId = 0; slotId < getActiveSimCount(); slotId++) {
+                        if ((mRadioGroup.getChildAt(slotId).getId() == checkedId)) {
+                            int[] subId = SubscriptionManager.getSubId(slotId);
+                            SubscriptionManager.from(mContext).setDefaultDataSubId(subId[0]);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private int getActiveSimCount() {
+        int activeSimCount = 0;
+        TelephonyManager telephonyManager =
+                (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        for (int i =0 ; i < telephonyManager.getPhoneCount(); i++) {
+            if (telephonyManager.getSimState(i) == TelephonyManager.SIM_STATE_READY) {
+                activeSimCount++;
+            }
+        }
+        return activeSimCount;
+    }
+
+    private String getCarrierName(int slotId) {
+        SubscriptionManager sm = SubscriptionManager.from(mContext);
+        List<SubscriptionInfo> subs = sm.getActiveSubscriptionInfoList();
+        CharSequence carrierName = subs.get(slotId).getCarrierName();
+        return carrierName.toString();
     }
 
     private String formatBytes(long bytes) {
