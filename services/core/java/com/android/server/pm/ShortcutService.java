fc1eb129ef7208592eff252542e8f2b22402a48d
XiNGRZ <chenxingyu92@gmail.com>
2017-11-08 20:21:58 +0800

    Keyguard: Add fingerprint shortcuts support [1/3]
    Unlocks the phone with a specific finger to launch a specific app
    
    Change-Id: Iefda5de64dd6bc8810db7e923071f329d2ab28f5

--- services/core/java/com/android/server/pm/ShortcutService.java
+++ services/core/java/com/android/server/pm/ShortcutService.java
@@ -2505,6 +2505,11 @@ public class ShortcutService extends IShortcutService.Stub {
         @Override
         public boolean hasShortcutHostPermission(int launcherUserId,
                 @NonNull String callingPackage) {
+            // Hack to allow shortcuts be started via fingerprint unlock
+            if ("com.android.systemui".equals(callingPackage) ||
+                    "org.mokee.mkparts".equals(callingPackage)) {
+                return true;
+            }
             return ShortcutService.this.hasShortcutHostPermission(callingPackage, launcherUserId);
         }
     }
