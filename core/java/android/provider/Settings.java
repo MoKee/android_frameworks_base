6e5935ce8fbdfc4aff513bce0e2d857ce0450b88
martincz <martincz@mokeedev.com>
2018-01-15 09:12:52 +0800

    base: Call log delete limit (2/2)
    Change-Id: Ie91a75742186f855c7ff0fd9d0a2dc98e3f836f2

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2006 The Android Open Source Project
- * Copyright (C) 2012-2018 The MoKee Open Source Project
+ * Copyright (C) 2012-2019 The MoKee Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -4095,6 +4095,11 @@ public final class Settings {
          * @hide
          */
         public static final String ENABLE_CLOUD_LOCATION_LOOKUP = "enable_cloud_location_lookup";
+
+        /**
+         * Call log delete limit
+         */
+        public static final String CALL_LOG_DELETE_LIMIT = "call_log_delete_limit";
 
         /**
          * Setting to determine whether or not to show the battery percentage in the status bar.
