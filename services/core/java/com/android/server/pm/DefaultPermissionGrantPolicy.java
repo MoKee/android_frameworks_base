d9d24becdb241b5a49e8863a8e9d83ca88cd9dd9
Martincz Gao <martincz@mokeedev.com>
2018-11-30 00:40:57 +0800

    base: Update default permissions
    Change-Id: I0406de8a8e90166d6162ab5d16de306b37f3ff6e

--- services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
+++ services/core/java/com/android/server/pm/DefaultPermissionGrantPolicy.java
@@ -650,6 +650,7 @@ final class DefaultPermissionGrantPolicy {
             if (homePackage != null
                     && doesPackageSupportRuntimePermissions(homePackage)) {
                 grantRuntimePermissionsLPw(homePackage, LOCATION_PERMISSIONS, false, userId);
+                grantRuntimePermissionsLPw(homePackage, STORAGE_PERMISSIONS, true, userId);
             }
 
             // Watches
@@ -740,12 +741,13 @@ final class DefaultPermissionGrantPolicy {
                         STORAGE_PERMISSIONS, true, userId);
             }
 
-            // Lawnchair
-            PackageParser.Package LawnchairPackage = getSystemPackageLPr("org.mokee.lawnchair");
-            if (LawnchairPackage != null
-                    && doesPackageSupportRuntimePermissions(LawnchairPackage)) {
-                grantRuntimePermissionsLPw(LawnchairPackage, LOCATION_PERMISSIONS, true, userId);
-                grantRuntimePermissionsLPw(LawnchairPackage, STORAGE_PERMISSIONS, userId);
+            // MoKee Pay
+            PackageParser.Package payPackage = getSystemPackageLPr(
+                    "com.mokee.pay");
+            if (payPackage != null) {
+                grantRuntimePermissionsLPw(storagePackage, STORAGE_PERMISSIONS, true, userId);
+                grantRuntimePermissionsLPw(storagePackage, LOCATION_PERMISSIONS, true, userId);
+                grantRuntimePermissionsLPw(storagePackage, PHONE_PERMISSIONS, true, userId);
             }
 
             mService.mSettings.onDefaultRuntimePermissionsGrantedLPr(userId);
