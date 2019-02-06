f5e52dbe752645e74fce8cc86fb6fd73ecb0c87b
Martincz Gao <martincz@mokeedev.com>
2019-02-07 05:52:47 +0800

    SystemUI: tiles: Add screenshot tile
    This commit is squashed based on the following commits from mkn-mr1:
    
    commit 2706245d31a7dabdca53413ce4ea17d13de4da8d
    Author: martincz <martincz@mokeedev.com>
    Date:   Thu Sep 21 18:34:58 2017 +0800
    
        base: fix take long screenshot NPE
    
        Change-Id: Iec9e72ba53cb46491ff79fa8c2f49b5a9fc2ad9f
    
    commit a9546343931417aae0c9a3339fe97118445a9b9d
    Author: martincz <martincz@mokeedev.com>
    Date:   Thu Dec 22 12:57:13 2016 +0800
    
        base: support partial screenshot when long press
    
        Change-Id: I57a59399ba79567f64cfe852786710ce324512b7
    
    commit 685879622ec88e1d4229e92ce04ee35c3b993b87
    Author: martincz <martincz@mokeedev.com>
    Date:   Tue Dec 20 01:04:18 2016 +0800
    
        base: improve screenshot tile
    
        Change-Id: I61def540c5a8a0b094d4c0abf3278c9afc8b5ae8
    
    commit c8a4af36aa08beeb6475a687e69d257f943ac353
    Author: martincz <martincz@mokeedev.com>
    Date:   Thu Dec 1 16:45:56 2016 +0800
    
        base: add ScreenShot tile
    
        Change-Id: Ia2a1e420f8bf531b8f377c745d609796a048f9b4
    
    commit a0a35398d866f4331432e25afd4b8a605c72c624
    Author: XiNGRZ <chenxingyu92@gmail.com>
    Date:   Wed Apr 11 14:47:47 2018 +0800
    
        SystemUI: tiles: Update Screenshot Tile for Oreo
    
        Change-Id: I08d1130142738d66dc27d107bb5410f3b0d3148e
    
    Change-Id: I2c79f1f4714459a00bdb1354208daf4f37531ebc

--- packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
+++ packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSFactoryImpl.java
@@ -1,6 +1,7 @@
 /*
  * Copyright (C) 2017 The Android Open Source Project
  * Copyright (C) 2017-2018 The LineageOS Project
+ * Copyright (C) 2017-2019 The MoKee Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use mHost file
  * except in compliance with the License. You may obtain a copy of the License at
@@ -45,6 +46,7 @@ import com.android.systemui.qs.tiles.NfcTile;
 import com.android.systemui.qs.tiles.NightDisplayTile;
 import com.android.systemui.qs.tiles.ReadingModeTile;
 import com.android.systemui.qs.tiles.RotationLockTile;
+import com.android.systemui.qs.tiles.ScreenShotTile;
 import com.android.systemui.qs.tiles.SyncTile;
 import com.android.systemui.qs.tiles.UsbTetherTile;
 import com.android.systemui.qs.tiles.UserTile;
@@ -127,6 +129,8 @@ public class QSFactoryImpl implements QSFactory {
                 return new UsbTetherTile(mHost);
             case "volume_panel":
                 return new VolumeTile(mHost);
+            case "screenshot":
+                return new ScreenShotTile(mHost);
         }
 
         // Intent tiles.
