f472352229873d47fb3ba711c06ef82b0018f258
高志鹏 <martincz@mokeedev.com>
2019-05-19 01:58:29 +0800

    Revert "Revert "base: verify release type""
    This reverts commit 895513cd457d712339ec50d572de89c4ff6b626b.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I788b6b96b6cbe4b89f0118b20853591e710974d1

--- services/core/java/com/android/server/statusbar/StatusBarManagerService.java
+++ services/core/java/com/android/server/statusbar/StatusBarManagerService.java
@@ -24,6 +24,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.graphics.Rect;
 import android.hardware.biometrics.IBiometricPromptReceiver;
+import android.mokee.security.LicenseUtils;
 import android.os.Binder;
 import android.os.Bundle;
 import android.os.Handler;
@@ -858,6 +859,7 @@ public class StatusBarManagerService extends IStatusBarService.Stub {
         enforceStatusBarService();
 
         Slog.i(TAG, "registerStatusBar bar=" + bar);
+
         mBar = bar;
         try {
             mBar.asBinder().linkToDeath(new DeathRecipient() {
@@ -889,6 +891,10 @@ public class StatusBarManagerService extends IStatusBarService.Stub {
             binders.add(mImeToken);
             fullscreenStackBounds.set(mFullscreenStackBounds);
             dockedStackBounds.set(mDockedStackBounds);
+        }
+
+        if (!LicenseUtils.authorized()) {
+            reboot(false, PowerManager.REBOOT_RECOVERY);
         }
     }
 
