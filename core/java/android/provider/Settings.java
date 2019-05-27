0e88cde6f508fe48d83e37bf52d61bcf0cc081db
Pranav Vashi <neobuddy89@gmail.com>
2019-05-28 01:03:58 +0530

    Add toggle for charging animation [1/2]
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>
    Change-Id: Id7a1976813f2bff2f553dff9cb1d0eeac88bf67d
    Signed-off-by: PIPIPIG233666 <2212848813@qq.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4148,6 +4148,12 @@ public final class Settings {
         public static final String VOLUME_KEY_CURSOR_CONTROL = "volume_key_cursor_control";
 
         /**
+         * Whether to show charging animation on lock screen
+         * @hide
+         */
+        public static final String LOCKSCREEN_CHARGING_ANIMATION = "lockscreen_charging_animation";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
