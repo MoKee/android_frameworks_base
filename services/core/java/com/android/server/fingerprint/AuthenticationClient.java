26cb31c8f68f9bb4272a49fec55df19365d2e840
高志鹏 <martincz@mokeedev.com>
2019-07-17 22:41:14 +0800

    Revert "Initial support of in-display fingerprint sensor"
    This reverts commit 7c81659f2d03cc85dd5a6bf4a5f67933db0ee8c5.
    
    Reason for revert: <INSERT REASONING HERE>
    
    Change-Id: I93e1a2661109ff14c2f7b2c5e83888ed00c1ce6e

--- services/core/java/com/android/server/fingerprint/AuthenticationClient.java
+++ services/core/java/com/android/server/fingerprint/AuthenticationClient.java
@@ -54,8 +54,6 @@ public abstract class AuthenticationClient extends ClientMonitor {
     private final FingerprintManager mFingerprintManager;
     protected boolean mDialogDismissed;
 
-    private FacolaView mFacola;
-
     // Receives events from SystemUI and handles them before forwarding them to FingerprintDialog
     protected IBiometricPromptReceiver mDialogReceiver = new IBiometricPromptReceiver.Stub() {
         @Override // binder call
@@ -98,7 +96,6 @@ public abstract class AuthenticationClient extends ClientMonitor {
         mStatusBarService = statusBarService;
         mFingerprintManager = (FingerprintManager) getContext()
                 .getSystemService(Context.FINGERPRINT_SERVICE);
-        mFacola = new FacolaView(context);
     }
 
     @Override
@@ -236,7 +233,6 @@ public abstract class AuthenticationClient extends ClientMonitor {
             resetFailedAttempts();
             onStop();
         }
-        if(result == true) mFacola.hide();
         return result;
     }
 
@@ -250,7 +246,6 @@ public abstract class AuthenticationClient extends ClientMonitor {
             Slog.w(TAG, "start authentication: no fingerprint HAL!");
             return ERROR_ESRCH;
         }
-        mFacola.show();
         onStart();
         try {
             final int result = daemon.authenticate(mOpId, getGroupId());
@@ -284,7 +279,6 @@ public abstract class AuthenticationClient extends ClientMonitor {
             return 0;
         }
 
-        mFacola.hide();
         onStop();
         IBiometricsFingerprint daemon = getFingerprintDaemon();
         if (daemon == null) {
