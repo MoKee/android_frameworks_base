1b7a23f6bd882228d90aef86e680a5d2f6500b5d
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-08-10 22:06:22 -0300

    base: Port extended screenshot function from OOS
    Change-Id: I856597056cb56d479a7a2c47dfdfc199da4302b8

--- packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
+++ packages/SystemUI/src/com/android/systemui/globalactions/GlobalActionsDialog.java
@@ -765,7 +765,11 @@ public class GlobalActionsDialog implements DialogInterface.OnDismissListener,
             mHandler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    mScreenshotHelper.takeScreenshot(1, true, true, mHandler);
+                    try {
+                        WindowManagerGlobal.getWindowManagerService().mokeeTakeScreenshot(1);
+                    } catch (RemoteException e) {
+                        Log.e(TAG, "Error while trying to take screenshot.", e);
+                    }
                     MetricsLogger.action(mContext,
                             MetricsEvent.ACTION_SCREENSHOT_POWER_MENU);
                 }
