8d315df61603f141ea0a53b3e7886a7ea6157b42
Martincz Gao <martincz@mokeedev.com>
2019-10-17 06:33:48 +0800

    base: show battery percent
    Change-Id: Ib8a9dc7e6325293e66290a37dde2105e0e69c2b7

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -384,7 +384,7 @@ public class BatteryMeterView extends LinearLayout implements
         final boolean showing = mBatteryPercentView != null;
         final boolean systemSetting = 0 != Settings.System
                 .getIntForUser(getContext().getContentResolver(),
-                SHOW_BATTERY_PERCENT, 0, mUser);
+                SHOW_BATTERY_PERCENT, 1, mUser);
 
         if ((mShowPercentAvailable && systemSetting && mShowPercentMode != MODE_OFF)
                 || mShowPercentMode == MODE_ON || mShowPercentMode == MODE_ESTIMATE) {
